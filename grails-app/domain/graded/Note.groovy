efe2d4f7547047caf8131066974703033dba1ac5
Carlo Baumann <carlo.baumann@students.fhnw.ch>
2019-11-19 08:52:08 +0100

    Constraints für Note

--- grails-app/domain/graded/Note.groovy
+++ grails-app/domain/graded/Note.groovy
@@ -3,8 +3,12 @@ package graded
 class Note {
 
     double note;
+    
+    // Gewichtung in Prozent Werten
     double gewichtung;
 
     static constraints = {
+        gewichtung(min: 0.0, max: 1.0)
+        note(min: 1.0, max: 6.0)
     }
 }
