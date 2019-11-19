441220869ec802962bc389dfe11702175c6d7988
Carlo Baumann <carlo.baumann@students.fhnw.ch>
2019-11-19 08:53:22 +0100

    Constraints für Semester

--- grails-app/domain/graded/Semester.groovy
+++ grails-app/domain/graded/Semester.groovy
@@ -1,10 +1,15 @@
 package graded
 
+import java.awt.print.Book
+
 class Semester {
 
     SemesterTyp semesterTyp;
     int jahr;
 
+    // static hasMany = [module: Modul]
+
     static constraints = {
+        jahr(min: 10, max: 99)
     }
 }
