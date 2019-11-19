7933b297f92048be6796ef012acfeb644122c09c
Carlo Baumann <carlo.baumann@students.fhnw.ch>
2019-11-19 19:58:43 +0100

    Mehr Default Daten

--- grails-app/init/graded/BootStrap.groovy
+++ grails-app/init/graded/BootStrap.groovy
@@ -14,6 +14,7 @@ class BootStrap {
 
         Modul algd1 = Modul.create()
         algd1.setModulKuerzel('algd1')
+        algd1.setModulBezeichnung('Algorithmen und Datenstrukturen 1')
         algd1.setCredits(3)
         algd1.setSemester(hs19)
 
@@ -21,16 +22,54 @@ class BootStrap {
         n1.setNote((double) 5.0)
         n1.setGewichtung((double) 1.0)
 
+
+        Note n2 = Note.create()
+        n2.setNote((double) 4.4)
+        n2.setGewichtung((double) 1.0)
+        Note.saveAll(n1, n2)
+
         Note msp = Note.create()
         msp.setNote((double) 4.0)
         msp.setGewichtung((double) 1.0)
 
         Set<Note> ens = new HashSet<>()
-        ens.add(n1);
-        Modul.saveAll(ens)
+        ens.add(n1)
+        ens.add(n2)
         algd1.setEns(ens)
         algd1.setMsp(msp)
         modulService.save(algd1)
+
+        Modul webeC = Modul.create()
+        webeC.setModulKuerzel('webeC')
+        webeC.setModulBezeichnung('Web Engineering')
+        webeC.setCredits(3)
+        webeC.setSemester(hs19)
+
+        Note n3 = Note.create()
+        n3.setNote((double) 4.0)
+        n3.setGewichtung((double) 1.0)
+        Note.saveAll(n3)
+
+        Set<Note> ens2 = new HashSet<>()
+        ens2.add(n3)
+        webeC.setEns(ens2)
+        modulService.save(webeC)
+
+        Modul webfr = Modul.create()
+        webfr.setModulKuerzel('webfr')
+        webfr.setModulBezeichnung('Web Frameworks')
+        webfr.setCredits(3)
+        webfr.setSemester(hs19)
+        modulService.save(webfr)
+
+        Modul agrh = Modul.create()
+        agrh.setModulKuerzel('agrh')
+        agrh.setModulBezeichnung('Argumentation und Rethorik')
+        agrh.setCredits(2)
+        agrh.setIsTestat(true)
+        agrh.setSemester(hs19)
+        modulService.save(agrh)
+
 
     }
 
