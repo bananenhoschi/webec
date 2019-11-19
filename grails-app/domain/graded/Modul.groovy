78d0a02dea3ef8a8a3850f057bd4cdcb1459eae3
Carlo Baumann <carlo.baumann@students.fhnw.ch>
2019-11-19 08:34:46 +0100

    Association auskommentiert. Test läuft

--- grails-app/domain/graded/Modul.groovy
+++ grails-app/domain/graded/Modul.groovy
@@ -1,10 +1,20 @@
 package graded
 
+import grails.rest.Resource
+
+@Resource(uri = '/moduls', formats = ['json'])
 class Modul {
 
     String modulKuerzel;
     String modulBezeichnung;
     String dozent;
+    int credits;
+
+    boolean hasMsp;
+    boolean isTestat;
+
+    // static hasMany = [en: Note]
+    // static hasOne = [msp: Note]
 
     static constraints = {
     }
