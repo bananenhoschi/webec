6e3d77d5f10f63f45b29e5ea128365eddba8d789
Carlo Eggenschwiler <carlo.eggenschwiler@students.fhnw.ch>
2019-12-09 21:59:33 +0100

    Added User

--- grails-app/init/graded/BootStrap.groovy
+++ grails-app/init/graded/BootStrap.groovy
@@ -9,12 +9,15 @@ class BootStrap {
     def init = { servletContext ->
         Role adminRole = new Role(authority: 'ROLE_ADMIN').save(failOnError: true)
         Role studentRole = new Role(authority: 'ROLE_STUDENT').save(failOnError: true)
+        Role guestRole = new Role(authority: 'ROLE_GUEST').save(failOnError: true)
 
         User user = new User(username: 'user', password: 'user').save(failOnError: true)
         User admin = new User(username: 'admin', password: 'admin').save(failOnError: true)
+        User guest = new User(username: 'guest', password: 'guest').save(failOnError: true)
 
         securityService.create(user, studentRole)
         securityService.create(admin, adminRole)
+        securityService.create(guest, guestRole)
 
         // ----------------------
 
